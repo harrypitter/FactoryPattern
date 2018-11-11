11377e2fd98e373c7f8fb75ad77849f6cf44b3b1
lidu <li.du@taosecret.com>
2018-11-11 12:23:24 +0800

    工程方法调整

--- projectStudy/src/Demo/DemoTestFactory.java
+++ projectStudy/src/Demo/DemoTestFactory.java
@@ -2,6 +2,10 @@ package Demo;
 
 import Fatcory.Factory;
 import Fatcory.Fruit;
+import Sort.BubleSort;
+import Sort.QuickSort;
+
+import java.util.ArrayList;
 
 /**
  * Created by Lidu on 18/4/21.
@@ -27,5 +31,28 @@ public class DemoTestFactory {
 
         //调用 grapeFruit 的 showInf 方法
         fruit3.showInf();
+
+        BubleSort bubleSort = new BubleSort();
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(0,1);
+        arrayList.add(1,5);
+        arrayList.add(2,9);
+        arrayList.add(3,789);
+        arrayList.add(4,87);
+        arrayList.add(5,56);
+        arrayList.add(6,-45);
+        arrayList.add(7,99);
+
+        bubleSort.getBubleSortData(arrayList);
+
+        int []a ={4,5,7,2,74,5,3,9,22,132};
+        int i;
+        QuickSort quickSort = new QuickSort();
+        //quickSort.quitSort(a,0,a.length-1);
+        quickSort.sort1(a,0,a.length-1);
+        for(i=0;i<a.length;i++)
+        {
+            System.out.printf("%d,",a[i]);
+        }
     }
 }
